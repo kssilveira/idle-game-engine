7d93e2588874a62ad346afaa3fa871f8540739ec
Kaue Silveira <silveira.kaue@gmail.com>
2023-11-26 21:11:24 +0100

    move comments to kittens

--- kittens/kittens.go
+++ kittens/kittens.go
@@ -54,3 +54,41 @@ func Run(logger *log.Logger, input Input) {
 		}
 	}
 }
+
+/*
+
+actions
+
+- "Gather catnip" add 1 catnip
+- "Refine catnip" cost 100 catnip add 1 wood
+
+buildings
+
+- count X resource X cost X time Xh (over cap)
+  - sell for half price
+- "Catnip Field" cost 10 catnip add 0.63 catnip/s
+	- cost 10 11.20 12.54 14.05 15.74 17.62
+	- add
+		- winter -75% 0.16/s
+		- spring +50% 0.94/s
+
+- "Hut" cost 5 wood add 2 kitten
+  - cost 10 12.5 31.25 78.13 195.31 488.28
+
+resources
+
+- count X cap X rate X/s to cap Xh
+- catnip cap 5000
+- wood cap 200
+- kitten cap 0
+  - eat 4.25 catnip/s
+
+jobs
+
+woodcuttter 0.09 wood/s
+  - reduced by happiness
+
+happiness
+- kittens 5 => 100% 98% 96% 94% 92% 90%
+
+*/
